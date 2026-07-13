654578f0aa44555982fe546fdab4f0ae08ade3a0
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2026-07-13 11:39:19 +0530

    added agent.md

---
+++ src/components/svg/Clock.tsx
@@ -0,0 +1,125 @@
+export default function Clock() {
+  return (
+    <div>
+      <svg
+        className={className}
+        fill="none"
+        preserveAspectRatio="xMidYMid meet" viewBox="0 0 372 372"
+      >
+        <g id="clock">
+          <g id="clock-frame">
+            <circle id="Ellipse 4" cx="186.5" cy="185.5" r="170.5" fill="#B2AED1" />
+            <ellipse id="Ellipse 3" cx="186.5" cy="187" rx="170.5" ry="169" fill="black" fill-opacity="0.84" />
+            <ellipse id="Ellipse 7" cx="186" cy="187.5" rx="153" ry="154.5" fill="url(#paint0_linear_2153_205)" />
+            <circle id="Ellipse 8" cx="186" cy="186" r="153" fill="black" />
+            <circle id="Ellipse 10" cx="186" cy="186" r="143.5" stroke="url(#paint1_linear_2153_205)" stroke-opacity="0.39" />
+            <path id="glow" opacity="0.46" d="M357 188.5C357 232.924 339.037 275.528 307.062 306.94C275.087 338.353 231.719 356 186.5 356C141.281 356 97.9133 338.353 65.9383 306.94C33.9634 275.528 16 232.924 16 188.5L186.5 188.5H357Z" fill="url(#paint2_linear_2153_205)" />
+          </g>
+          <g id="clock-mins">
+            <g id="Group 2">
+              <rect id="Rectangle 1" x="184.469" y="54.4686" width="2" height="9" fill="#D9D9D9" />
+              <rect id="Rectangle 2" x="184.469" y="308.469" width="2" height="9" fill="#D9D9D9" />
+              <rect id="Rectangle 3" x="317.469" y="184.469" width="2" height="9" transform="rotate(90 317.469 184.469)" fill="#D9D9D9" />
+              <rect id="Rectangle 4" x="63.4688" y="185.469" width="2" height="9" transform="rotate(90 63.4688 185.469)" fill="#D9D9D9" />
+            </g>
+            <g id="Group 3">
+              <rect id="Rectangle 1_2" x="163.417" y="56.4081" width="2" height="9" transform="rotate(-9.22094 163.417 56.4081)" fill="#2B2B2B" />
+              <rect id="Rectangle 2_2" x="204.118" y="307.126" width="2" height="9" transform="rotate(-9.22094 204.118 307.126)" fill="#2B2B2B" />
+              <rect id="Rectangle 3_2" x="315.53" y="163.416" width="2" height="9" transform="rotate(80.7791 315.53 163.416)" fill="#2B2B2B" />
+              <rect id="Rectangle 4_2" x="64.9717" y="205.105" width="2" height="9" transform="rotate(80.7791 64.9717 205.105)" fill="#2B2B2B" />
+            </g>
+            <g id="Group 4">
+              <rect id="Rectangle 1_3" x="144.43" y="61.1928" width="2" height="9" transform="rotate(-17.7599 144.43 61.1928)" fill="#2B2B2B" />
+              <rect id="Rectangle 2_3" x="221.907" y="303.088" width="2" height="9" transform="rotate(-17.7599 221.907 303.088)" fill="#2B2B2B" />
+              <rect id="Rectangle 3_3" x="310.745" y="144.429" width="2" height="9" transform="rotate(72.2401 310.745 144.429)" fill="#2B2B2B" />
+              <rect id="Rectangle 4_3" x="69.1543" y="222.859" width="2" height="9" transform="rotate(72.2401 69.1543 222.859)" fill="#2B2B2B" />
+            </g>
+            <g id="Group 5">
+              <rect id="Rectangle 1_4" x="125.145" y="69.371" width="2" height="9" transform="rotate(-26.8957 125.145 69.371)" fill="#2B2B2B" />
+              <rect id="Rectangle 2_4" x="240.047" y="295.896" width="2" height="9" transform="rotate(-26.8957 240.047 295.896)" fill="#2B2B2B" />
+              <rect id="Rectangle 3_4" x="302.567" y="125.144" width="2" height="9" transform="rotate(63.1043 302.567 125.144)" fill="#2B2B2B" />
+              <rect id="Rectangle 4_4" x="76.4932" y="240.938" width="2" height="9" transform="rotate(63.1043 76.4932 240.938)" fill="#2B2B2B" />
+            </g>
+            <g id="Group 6">
+              <rect id="Rectangle 1_5" x="107.404" y="80.5074" width="2" height="9" transform="rotate(-36.0315 107.404 80.5074)" fill="#2B2B2B" />
+              <rect id="Rectangle 2_5" x="256.814" y="285.916" width="2" height="9" transform="rotate(-36.0315 256.814 285.916)" fill="#2B2B2B" />
+              <rect id="Rectangle 3_5" x="291.43" y="107.403" width="2" height="9" transform="rotate(53.9685 291.43 107.403)" fill="#2B2B2B" />
+              <rect id="Rectangle 4_5" x="86.6094" y="257.623" width="2" height="9" transform="rotate(53.9685 86.6094 257.623)" fill="#2B2B2B" />
+            </g>
+            <g id="Group 7">
+              <rect id="Rectangle 1_6" x="91.6555" y="94.3196" width="2" height="9" transform="rotate(-45.1673 91.6555 94.3196)" fill="white" />
+              <rect id="Rectangle 2_6" x="271.784" y="273.4" width="2" height="9" transform="rotate(-45.1673 271.784 273.4)" fill="white" />
+              <rect id="Rectangle 3_6" x="277.618" y="91.6552" width="2" height="9" transform="rotate(44.8327 277.618 91.6552)" fill="white" />
+              <rect id="Rectangle 4_6" x="99.2463" y="272.49" width="2" height="9" transform="rotate(44.8327 99.2463 272.49)" fill="white" />
+            </g>
+            <g id="Group 8">
+              <rect id="Rectangle 1_7" x="78.3003" y="110.457" width="2" height="9" transform="rotate(-54.3031 78.3003 110.457)" fill="#2B2B2B" />
+              <rect id="Rectangle 2_7" x="284.578" y="258.665" width="2" height="9" transform="rotate(-54.3031 284.578 258.665)" fill="#2B2B2B" />
+              <rect id="Rectangle 3_7" x="261.481" y="78.3" width="2" height="9" transform="rotate(35.6969 261.481 78.3)" fill="#2B2B2B" />
+              <rect id="Rectangle 4_7" x="114.084" y="285.161" width="2" height="9" transform="rotate(35.6969 114.084 285.161)" fill="#2B2B2B" />
+            </g>
+            <g id="Group 9">
+              <rect id="Rectangle 1_8" x="67.6765" y="128.51" width="2" height="9" transform="rotate(-63.4389 67.6765 128.51)" fill="#2B2B2B" />
+              <rect id="Rectangle 2_8" x="294.869" y="242.087" width="2" height="9" transform="rotate(-63.4389 294.869 242.087)" fill="#2B2B2B" />
+              <rect id="Rectangle 3_8" x="243.427" y="67.6766" width="2" height="9" transform="rotate(26.5611 243.427 67.6766)" fill="#2B2B2B" />
+              <rect id="Rectangle 4_8" x="130.745" y="295.317" width="2" height="9" transform="rotate(26.5611 130.745 295.317)" fill="#2B2B2B" />
+            </g>
+            <g id="Group 10">
+              <rect id="Rectangle 1_9" x="60.054" y="148.021" width="2" height="9" transform="rotate(-72.5747 60.054 148.021)" fill="#2B2B2B" />
+              <rect id="Rectangle 2_9" x="302.397" y="224.084" width="2" height="9" transform="rotate(-72.5747 302.397 224.084)" fill="#2B2B2B" />
+              <rect id="Rectangle 3_9" x="223.917" y="60.0542" width="2" height="9" transform="rotate(17.4253 223.917 60.0542)" fill="#2B2B2B" />
+              <rect id="Rectangle 4_9" x="148.807" y="302.698" width="2" height="9" transform="rotate(17.4253 148.807 302.698)" fill="#2B2B2B" />
+            </g>
+            <g id="Group 11">
+              <rect id="Rectangle 1_10" x="55.6257" y="168.494" width="2" height="9" transform="rotate(-81.7105 55.6257 168.494)" fill="#2B2B2B" />
+              <rect id="Rectangle 2_10" x="306.972" y="205.115" width="2" height="9" transform="rotate(-81.7105 306.972 205.115)" fill="#2B2B2B" />
+              <rect id="Rectangle 3_10" x="203.443" y="55.6265" width="2" height="9" transform="rotate(8.28952 203.443 55.6265)" fill="#2B2B2B" />
+              <rect id="Rectangle 4_10" x="167.812" y="307.118" width="2" height="9" transform="rotate(8.28952 167.812 307.118)" fill="#2B2B2B" />
+            </g>
+          </g>
+          <g id="clock-hands">
+            <g id="min-hand-with-speed-background">
+              <mask id="path-47-inside-1_2153_205" fill="white">
+                <path d="M73.9985 189C73.9985 174.292 76.8955 159.728 82.524 146.139C88.1525 132.551 96.4024 120.204 106.803 109.804C117.203 99.4038 129.55 91.1539 143.138 85.5254C156.726 79.8968 171.29 76.9999 185.999 76.9999L185.999 189H73.9985Z" />
+              </mask>
+              <path d="M73.9985 189C73.9985 174.292 76.8955 159.728 82.524 146.139C88.1525 132.551 96.4024 120.204 106.803 109.804C117.203 99.4038 129.55 91.1539 143.138 85.5254C156.726 79.8968 171.29 76.9999 185.999 76.9999L185.999 189H73.9985Z" fill="url(#paint3_linear_2153_205)" stroke="url(#paint4_linear_2153_205)" stroke-width="4" mask="url(#path-47-inside-1_2153_205)" />
+            </g>
+            <rect id="hour-hand" x="186.236" y="189.88" width="3" height="78.1619" transform="rotate(-82 186.236 189.88)" fill="url(#paint5_linear_2153_205)" />
+            <ellipse id="clock-center-nut" cx="185" cy="187.5" rx="15" ry="15.5" fill="#D9D9D9" />
+          </g>
+        </g>
+        <defs>
+          <linearGradient id="paint0_linear_2153_205" x1="182" y1="138.815" x2="185.949" y2="342.001" gradientUnits="userSpaceOnUse">
+            <stop stop-opacity="0" />
+            <stop offset="1" stop-color="#F2E6FC" />
+          </linearGradient>
+          <linearGradient id="paint1_linear_2153_205" x1="186" y1="159.5" x2="186" y2="330" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#707070" />
+            <stop offset="1" stop-opacity="0" />
+          </linearGradient>
+          <linearGradient id="paint2_linear_2153_205" x1="186.5" y1="188.5" x2="186.5" y2="356" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#180020" stop-opacity="0" />
+            <stop offset="0.387309" stop-color="#9000FF" stop-opacity="0.25" />
+            <stop offset="0.634615" stop-color="#A200FF" stop-opacity="0.634615" />
+            <stop offset="0.735577" stop-color="#B324FF" stop-opacity="0.735577" />
+            <stop offset="1" stop-color="#E497FF" />
+          </linearGradient>
+          <linearGradient id="paint3_linear_2153_205" x1="188.999" y1="89.4999" x2="131.999" y2="142.5" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" />
+            <stop offset="0.166553" stop-color="#FEC5FF" />
+            <stop offset="0.325583" stop-color="#A26BFF" />
+            <stop offset="1" stop-opacity="0" />
+          </linearGradient>
+          <linearGradient id="paint4_linear_2153_205" x1="185.999" y1="76.9999" x2="113.499" y2="157.5" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" />
+            <stop offset="1" stop-opacity="0" />
+          </linearGradient>
+          <linearGradient id="paint5_linear_2153_205" x1="187.99" y1="251.9" x2="187.758" y2="170.554" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#D9D9D9" />
+            <stop offset="1" stop-opacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  )
+}
